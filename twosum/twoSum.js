77efe36bb4dceb3d4c95b9570150a11d0f43c00c
numakuro-0xf00 <enheione1234@gmail.com>
2025-01-22 23:22:17 +0900

    Accepted

---
+++ twosum/twoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const hash = {};
+    let n = nums.length;
+    for (let i = 0; i < n; i++) {
+        const complement = target - nums[i];
+        if (complement in hash) {
+            return [hash[complement], i];
+        }hash[nums[i]] = i;
+    }return [];
+};
